58c941178f8820472ea8495fdb88f78b512e99cc
Hung820416 <a0982417707>
2026-07-12 20:19:56 +0800

    新增訪客計數器

--- js/script.js
+++ js/script.js
@@ -23,3 +23,152 @@ darkBtn.addEventListener("click", function() {
     document.body.classList.toggle("dark");
 
 });
+const topBtn = document.getElementById("topBtn");
+
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+
+
+topBtn.addEventListener("click", function(){
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
+window.addEventListener("load",function(){
+
+    const loader = document.getElementById("loader");
+
+
+    setTimeout(function(){
+
+        loader.style.display="none";
+
+    },1000);
+
+
+});
+const menuBtn =
+document.getElementById("menuBtn");
+
+
+const navMenu =
+document.getElementById("navMenu");
+
+
+
+menuBtn.addEventListener("click",function(){
+
+    navMenu.classList.toggle("active");
+
+});
+
+//作品分類
+
+
+const filterBtns =
+document.querySelectorAll(".filterBtn");
+
+
+const projects =
+document.querySelectorAll(".project");
+
+
+
+filterBtns.forEach(function(btn){
+
+
+btn.addEventListener("click",function(){
+
+
+
+let category=this.textContent;
+
+
+
+projects.forEach(function(project){
+
+
+if(category=="全部"){
+
+project.style.display="block";
+
+}
+
+
+else if(project.classList.contains(category.toLowerCase())){
+
+project.style.display="block";
+
+}
+
+
+else{
+
+project.style.display="none";
+
+}
+
+
+});
+
+
+
+});
+
+
+
+});
+// 訪客計數器
+
+
+let count = localStorage.getItem("visitors");
+
+
+
+if(count == null){
+
+    count = 1;
+
+}
+
+else{
+
+    count++;
+
+}
+
+
+
+localStorage.setItem("visitors", count);
+
+
+
+const visitor =
+document.getElementById("visitorCount");
+
+
+
+if(visitor){
+
+    visitor.textContent = count;
+
+}
